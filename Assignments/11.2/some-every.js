aa8e9ef1ac59768ac951be6a612afa42fb6636e5
mckaycjm21 <mckaycjm21@gmail.com>
2023-09-22 16:45:30 -0500

    Finished problems all test successful

--- Assignments/11.2/some-every.js
+++ Assignments/11.2/some-every.js
@@ -7,9 +7,9 @@ Examples:
 */
 
 function hasOddNumber(arr) {
-    arr.some(function(value){
-    return value % 2 !== 0;
-});
+    return arr.some(function(value){
+        return value % 2 !== 0;
+    });
 }
 
 /*
@@ -21,8 +21,8 @@ Examples:
 */
 
 function hasAZero(num) {
-    num.some(function(value){
-        return value == 0;
+    return num.toString().split('').some(function(value){
+        return value === '0';
     })
 }
 
@@ -34,7 +34,11 @@ Examples:
     hasOnlyOddNumbers([1,2,3,5,7]) // false
 */
 
-function hasOnlyOddNumbers(arr) {}
+function hasOnlyOddNumbers(arr) {
+    return arr.every(function(value){
+        return value % 2 !== 0;
+    });
+}
 
 /*
 Write a function called hasNoDuplicates which accepts an array and returns true if there are no duplicate values (more than one element in the array that has the same value as another). If there are any duplicates, the function should return false.
@@ -44,7 +48,11 @@ Examples:
     hasNoDuplicates([1,2,3]) // true
 */
 
-function hasNoDuplicates(arr) {}
+function hasNoDuplicates(arr) {
+    return arr.every( function(value, indx){
+        return arr.indexOf(value) === indx;
+    });
+}
 
 /*
 Write a function called hasCertainKey which accepts an array of objects and a key, and returns true if every single object in the array contains that key. Otherwise it should return false.
@@ -61,7 +69,11 @@ Examples:
     hasCertainKey(arr,'isCatOwner') // false
 */
 
-function hasCertainKey(arr, key) {}
+function hasCertainKey(arr, key) {
+    return arr.every(function(value){
+        return key in value;
+    })
+}
 
 /*
 Write a function called hasCertainValue which accepts an array of objects and a key, and a value, and returns true if every single object in the array contains that value for the specific key. Otherwise it should return false.
@@ -79,4 +91,8 @@ Examples:
     
 */
 
-function hasCertainValue(arr, key, searchValue) {}
+function hasCertainValue(arr, key, searchValue) {
+    return arr.every(function(value){
+        return searchValue === value[key];
+    })
+}
