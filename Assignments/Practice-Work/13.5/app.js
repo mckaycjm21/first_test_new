7652caeba259af5e5fd7682165efd774b40350db
mckaycjm21 <mckaycjm21@gmail.com>
2023-09-27 14:35:35 -0500

    finished 13.5

---
+++ Assignments/Practice-Work/13.5/app.js
@@ -0,0 +1,69 @@
+// function createInstructor(firstName, lastName){
+//     return {
+//       firstName: firstName,
+//       lastName: lastName
+//     }
+//   }
+
+  function createInstructor(firstName, lastName){
+    return{
+      firstName,
+      lastName
+    }
+  }
+
+
+// var favoriteNumber = 42;
+
+// var instructor = {
+//   firstName: "Colt"
+// }
+
+// instructor[favoriteNumber] = "That is my favorite!"
+
+
+let favoriteNumber = 42;
+let instructor = {
+  firstName : "Colt",
+  [favoriteNumber] : "This is my favorite"
+}
+
+
+// var instructor3 = {
+//   firstName: "Colt",
+//   sayHi: function(){
+//     return "Hi!";
+//   },
+//   sayBye: function(){
+//     return this.firstName + " says bye!";
+//   }
+// }
+
+
+let instructor2 = {
+  firstName : "Colt",
+  sayHi() {
+    return "Hi!";
+  },
+  sayBye() {
+    return this.firstName + " says bye!";
+  }
+}
+
+
+// const d = createAnimal("dog", "bark", "Woooof!")
+// // {species: "dog", bark: ƒ}
+// d.bark()  //"Woooof!"
+
+// const s = createAnimal("sheep", "bleet", "BAAAAaaaa")
+// // {species: "sheep", bleet: ƒ}
+// s.bleet() //"BAAAAaaaa"
+
+
+const animalObj  = (species, verb, noise) => {
+  return {
+    species,
+    verb,
+    noise
+  }
+}
