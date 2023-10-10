0819a6d4ca7a6e5a78216d98d02160bf36b9fc7a
mckaycjm21 <mckaycjm21@gmail.com>
2023-10-09 19:41:11 -0500

    finish search suggestion minus css

--- Assignments/autocomplete_starter_code/starter_code/script.js
+++ Assignments/autocomplete_starter_code/starter_code/script.js
@@ -1,36 +1,35 @@
-let input = document.querySelector('#fruit');
+let inputElement = document.querySelector('#fruit');
 const suggestions = document.querySelector('.suggestions ul');
-let makeLi  = document.createElement("li");
-let permList = [];
-
+//const listItemElement = document.createElement('li');
 
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
 function search(str) {
 	let results = [];
-	let lowerCaseChar = "";
-	let userInputHere = ""
-	for(let i = 0; i < str.length; i++){
-		lowerCaseChar = str.toLowerCase();
-		userInputHere[i] = lowerCaseChar;
-		console.log(userInputHere);
-	}
-	for (let test of fruit){
-		let lowerCaseFruit = test.toLowerCase();
-		if (lowerCaseFruit.includes(lowerCaseChar)){
-			results.push(lowerCaseFruit);
+	for (let fruitName of fruit){
+		let lowerCaseFruitName = fruitName.toLowerCase();		
+		if (lowerCaseFruitName.includes(str.toLowerCase())){
+		results.push(fruitName);
 		}
 	}
-
 	return results;
 }
 
 function searchHandler(e) {
-	let results = search(input.value);
-	console.log(results);
+	let results = search(inputElement.value);
+	showSuggestions(results)
 }
 
 function showSuggestions(results, inputVal) {
+	suggestions.innerHTML = ''
+	console.log(results);
+	for(let result of results) {
+	const listItemElement = document.createElement('li');
+	listItemElement.textContent = result;
+	suggestions.appendChild(listItemElement);		
+	}
+
+
 	//Take the Array from search handler and return an 
 }
 
@@ -38,5 +37,5 @@ function useSuggestion(e) {
 	// onMouseClick input value from li into search text box value
 }
 
-input.addEventListener('keyup', searchHandler);
+inputElement.addEventListener('keyup', searchHandler);
 suggestions.addEventListener('click', useSuggestion);
