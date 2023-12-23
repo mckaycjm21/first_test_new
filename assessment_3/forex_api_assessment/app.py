ba16a93de89e75ce65b2d011038f6ca84d202e20
mckaycjm21 <mckaycjm21@gmail.com>
2023-12-23 17:50:56 -0600

    Completed 26.3

--- assessment_3/forex_api_assessment/app.py
+++ assessment_3/forex_api_assessment/app.py
@@ -23,8 +23,6 @@ class New_User(db.Model):
     last_name = db.Column(db.String(50))
     image_url = db.Column(db.String, unique = True)
 
-    posts = db.relationship('Post')
-
 
     def greet(self):
         return f"Hi my name is {self.first_name} {self.last_name}"
@@ -43,14 +41,29 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key = True, autoincrement = True)
     title = db.Column(db.String(50))
     content = db.Column(db.String)
-    created_at = db.Column(db.String)
+    created_at = db.Column(db.String, default = datetime.now())
     user_id = db.Column(db.Integer, db.ForeignKey("new_users.id"))
 
-    user_info = db.relationship('New_User')
+    user_info = db.relationship('New_User', backref='posts')
+    tags = db.relationship('Tag', secondary = 'poststags', backref = 'posts')
 
     def create_post(new_post):
         db.session.add(new_post)
         db.session.commit()
+
+
+class PostTag(db.Model):
+    __tablename__ = 'poststags'
+
+    post_id= db.Column(db.Integer, db.ForeignKey("posts.id"), primary_key = True)
+    tag_id= db.Column(db.Integer, db.ForeignKey("tags.id"), primary_key = True)
+
+class Tag(db.Model):
+    __tablename__ = "tags"
+
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    name = db.Column(db.String(50), unique = True)
+
 
 with app.app_context():
     db.create_all()
@@ -60,7 +73,8 @@ with app.app_context():
 @app.route("/")
 def user_form():
     all_users = New_User.query.all()
-    return render_template("landing.html", all_users = all_users)
+    tag = Tag.query.get(1)
+    return render_template("landing.html", all_users = all_users, tag = tag)
 
 @app.route("/add_user_form")
 def add_user_form():
@@ -123,7 +137,8 @@ def user_page(id):
 @app.route("/add_post_form/<id>")
 def add_post_form(id):
     user = New_User.query.get(id)
-    return render_template("add_post.html", user = user)
+    tags = Tag.query.all()
+    return render_template("add_post.html", user = user, tags = tags)
 
 @app.route("/add_post/<id>", methods = ["POST"])
 def add_post(id):
@@ -132,8 +147,14 @@ def add_post(id):
         post_content = request.form['content']
         post_title = request.form['title']
         post_created_at = datetime.now()
+        tags = request.form.getlist('tags')
         new_post = Post(title = post_title, content = post_content, created_at = post_created_at, user_id = id)
         Post.create_post(new_post)
+        if tags:
+            for tag in tags:
+                temp_post_tag = PostTag(post_id = new_post.id, tag_id = tag)
+                db.session.add(temp_post_tag)
+                db.session.commit()
         return redirect(f'/view_all_posts/{id}')
     else:
          return redirect("/error.html")
@@ -146,23 +167,34 @@ def view_post(id):
 @app.route("/view_all_posts/<id>", methods = ["GET", "POST"])
 def view_all_posts(id):
     posts = Post.query.filter_by(user_id = id).all()
+    tags = request.form.getlist('tag_name')
     if(request.method == 'GET'):
-        return render_template("all_posts.html", posts=posts)
+        
+        return render_template("all_posts.html", posts=posts, tags=tags)
     else:
-        return render_template("all_posts.html", posts=posts)
+        return render_template("all_posts.html", posts=posts, tags=tags)
 
 @app.route("/edit_post_form/<id>")
 def edit_post_form(id):
      post = Post.query.get(id)
-     return render_template("edit_post_form.html", post = post)
+     tags = Tag.query.all()
+     return render_template("edit_post_form.html", post = post, tags = tags)
 
 @app.route("/edit_post/<id>", methods = ['POST'])
 def edit_post(id):
     post = Post.query.get(id)
     post.title = request.form['title']
     post.content = request.form['content']
-    db.session.add(post)
-    db.session.commit()
+    all_post_tag = PostTag.query.filter_by(post_id = post.id).all()
+    tags = request.form.getlist('tags')
+    for tag in all_post_tag:
+        db.session.delete(tag)
+        db.session.commit()
+    for tag in tags:
+            temp_post_tag = PostTag(post_id = post.id, tag_id = tag)
+            db.session.add(temp_post_tag)
+            db.session.commit()
+
     return redirect(f"/view_all_posts/{post.user_id}")
 
 @app.route("/delete_post/<id>")
@@ -171,3 +203,41 @@ def delete_post(id):
      db.session.delete(post)
      db.session.commit()
      return redirect(f"/user{post.user_id}")
+
+@app.route("/tags")
+def tags_detail():
+    all_tags = Tag.query.all()
+    return render_template("all_tags.html", all_tags = all_tags)
+
+@app.route("/tags/<id>")
+def tag_detail(id):
+    tag = Tag.query.get(id)
+    return render_template("show_tag.html", tag = tag)
+
+@app.route("/tags/new", methods = ['POST', 'GET'])
+def create_tag():
+    if (request.method == 'GET'):
+        return render_template("create_tag.html")
+    else:
+        tag_name = request.form['tag_name']
+        db.session.add(Tag(name = tag_name))
+        db.session.commit()
+        return redirect("/")
+
+@app.route("/tags/<id>/edit", methods = ['GET', 'POST'])
+def show_edit_form(id):
+    tag = Tag.query.get(id)
+    if(request.method == "GET"):
+        return render_template("tag_edit_form.html", tag = tag)
+    else:
+        tag.name = request.form['tag_name']
+        db.session.add(tag)
+        db.session.commit()
+        return redirect('/')
+    
+@app.route("/tags/<id>/delete")
+def delete_tag(id):
+    tag = Tag.query.get(id)
+    db.session.delete(tag)
+    db.session.commit()
+    return redirect('/')
